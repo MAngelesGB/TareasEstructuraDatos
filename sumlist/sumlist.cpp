71447128b7440aac43ffd0e6df14631ccfe2d0a7
Angeles <90299385+MAngelesGB@users.noreply.github.com>
2024-08-01 21:01:10 -0600

    sum list with double lists

--- sumlist/sumlist.cpp
+++ sumlist/sumlist.cpp
@@ -4,48 +4,109 @@
 using namespace std; 
 
 struct Node{
+  Node *before; 
   int num;
   Node *next; 
 }; 
 
 Node* linkedList1();
 Node* linkedList2();
-Node* result(Node *list, int num); 
+Node* createList(Node *&list, int num); 
+Node* reverseList(Node *); 
+void print(Node *); 
 
 Node* linkedList1(){
-  Node *head = new Node{1, nullptr};
-  head->next = new Node{2, nullptr};
-  head->next->next = new Node{7, nullptr};
-  return head; 
+  Node *head = new Node{nullptr, 1, nullptr};
+
+  head->next = new Node{nullptr, 1, nullptr}; 
+  head->next->before = head;
+
+  head->next->next = new Node{nullptr, 1, nullptr};
+  head->next->next->before = head->next; 
+
+  return reverseList(head); 
 }
 
 Node* linkedList2(){
-  Node *head = new Node{9, nullptr};
-  head->next = new Node{8, nullptr}; 
-  return head; 
+  Node *head = new Node{nullptr, 8, nullptr};
+
+  head->next = new Node{nullptr, 5, nullptr}; 
+  head->next->before = head;
+  
+  return reverseList(head); 
 }
 
-Node* result(Node *list, int num){
-  Node *newNode = new Node{num, nullptr}; 
+Node* reverseList(Node *head){
+  Node *temp = head; 
+  while(temp->next != nullptr){
+    temp = temp->next;
+  }
+  return temp; 
+}
 
+Node* createList(Node *&list, int num){
+  Node *newNode = new Node{nullptr, num, nullptr};
+  
   if(list == nullptr){
     list = newNode;
   }else{
-    while(list->next != nullptr){
-      list = list ->next;
+    Node *temp = list;
+    while(temp->next != nullptr){
+      temp = temp->next;
     }
-    list->next = newNode; 
+    temp->next = newNode;
+    newNode->before = temp;
+  }
+  return list; 
+}
+
+void print(Node *list){
+  while(list != nullptr){
+    cout<< list->num << " "; 
+    list = list->before; 
+  }
+}
+
+void sumList(int sum, Node *aux, Node *&head){
+  if(sum > 9){
+    if(aux->before != nullptr){
+      int first = sum / 10;
+      int second = sum % 10;  
+      aux->before->num += first;
+      createList(head, second); 
+    }else{
+      createList(head, sum); 
+    }
+  }else{
+    createList(head, sum); 
   }
 }
 
 int main(){
-  Node *list = nullptr; 
-  Node *list1 = linkedList1(); 
-  Node *list2 = linkedList2(); 
+  int sum; 
+  Node *head = nullptr;
+  Node *aux1 = linkedList1(); 
+  Node *aux2 = linkedList2(); 
   bool flag = false;
 
+
   while(flag != true){
-    
+    if(aux1 != nullptr && aux2 != nullptr){
+      sum = aux1->num + aux2->num; 
+      sumList(sum, aux1, head); 
+    }else if(aux1 != nullptr && aux2 == nullptr){
+      createList(head, aux1->num); 
+    }else{
+      flag = true; 
+    }
+
+    if(aux1 != nullptr){
+      aux1 = aux1->before; 
+    }
+    if(aux2 != nullptr){
+      aux2 = aux2->before;
+    }
   }
-  return 0; 
+
+  print(reverseList(head)); 
 }
