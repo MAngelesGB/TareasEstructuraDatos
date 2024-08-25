f3cc5cee821a092d4dad37d95fc015d5ae808bcb
Angeles <90299385+MAngelesGB@users.noreply.github.com>
2024-08-24 20:43:23 -0600

    Correccion de error de tipo de dato

--- priorityqueue/pqAlarm.cpp
+++ priorityqueue/pqAlarm.cpp
@@ -5,39 +5,55 @@
 using namespace std;
 using namespace chrono;
 
+//Clase de alarma 
 class Alarm{
   public:
+  //Establece la estructura de la alarma
     struct Node{ 
       int duration;   
       int day;
       int hour;
-      Node(int dt, int d, int h):duration(dt), day(d), hour(h){}; 
+      Node(int dt, int d, int h):duration(dt), day(d), hour(h){}; //Constructor
     };
-    void addAlarm(int quantity);
-    Node nextAlarm();
+    void addAlarm(int quantity);//Agregar alarma 
+    Node nextAlarm(); //Obtener la alarma mas proxima
   private:
-    class Compare{
-    public:
-      bool operator()(const Alarm::Node& a, const Alarm::Node& b){
-        return a.duration > b.duration;
-      }
+    class Compare{ //Clase anidada para usarla a manera de comparador
+      public:
+        //implementacion de functor
+        bool operator()(const Alarm::Node& a,const Alarm::Node& b ){
+          
+          //en caso de que sean alarmas atrasadas se verifican cual de dos ocurrio antes
+          return a.duration > b.duration; 
+        }
     }; 
-    priority_queue<Node, vector<Node>, Compare> pq;
-    int randomDuration();
+    priority_queue<Node, vector<Node>, Compare> pq; //se establece la priority queue 
+    int randomDuration(); // una funcion que genera una duracion aleatorio
 };
 
 
 int Alarm::randomDuration()
 {
+  //consigue el epoch del tiempo actual
   auto tp = system_clock::now();
-  auto dt = duration_cast<seconds>(tp.time_since_epoch());
+  auto dt = duration_cast<seconds>(tp.time_since_epoch()).count();
+  int randNum = rand() % 2 + 1; //numero aleatorio para decidir
+  int randomdt; //almacena el tiempo aleatorio de la nueva alarma
 
-  int randomdt = dt.count()+ rand()%86400; 
-  return randomdt;
+  /*Utilizado para generar de manera dinamica
+  las alarmas proximas y pasadas apartir del tiempo actual
+  */ 
+  
+    randomdt = dt + rand()%259200-(259200/2); //suma al epoch actual 
+  return randomdt; 
 }
 
 void Alarm::addAlarm(int quantity)
 {
+  /*Utilizado para insertar un numero determinado de alarmas
+    en el que se establecen los valores de la estructura que se
+    indica en la clase.
+  */
   for(int i =0; i<= quantity; i++){
     //Almacena el tiempo del epoch para extraer dia y hora
     time_t timeStmp= randomDuration();
@@ -49,20 +65,45 @@ void Alarm::addAlarm(int quantity)
   }
 }
 
+
 Alarm::Node Alarm::nextAlarm(){
+
+  /*
+    Ya que solo regresa nodos, se coloca 
+    un mensaje de error para que no caiga
+    el programa
+   */
   if(pq.empty())
   {
+    //si esta vacia lanza un error
     throw runtime_error("pila vacia");
   
   }
-  Node topNode = pq.top();
-    pq.pop();
-    return topNode; 
+
+  /*Se hace este proceso para 
+  sacar la proxima alerta conforme las 
+  iteraciones. 
+  */
+
+  auto tp = system_clock::now();
+  auto dt = duration_cast<seconds>(tp.time_since_epoch()).count();
+
+  while(!(pq.empty()) && pq.top().duration < dt){
+    cout<<pq.size()<<endl; 
+    cout<< pq.top().duration - dt<<endl; 
+    cout<<"duracion pq"<<pq.top().duration<<" dt"<<dt<<endl;
+    pq.pop(); 
+  }
+  Node topNode = pq.top(); // se almacena la alarma mas proxima 
+  pq.pop(); //la elimina
+  return topNode; //regresa la alarma 
 }
 
 int main()
 {
-  srand(static_cast<unsigned>(time(0)));
   Alarm a;
-  a.addAlarm(10);
+  a.addAlarm(5);
+  auto alarm = a.nextAlarm(); 
+  cout<< alarm.duration; 
+
 }
