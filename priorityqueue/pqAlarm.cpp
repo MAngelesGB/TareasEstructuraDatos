2ba9b71087015c9cdc3a82984d7ca3ee8099468b
Angeles <90299385+MAngelesGB@users.noreply.github.com>
2024-08-24 22:04:34 -0600

    Add files via upload

--- priorityqueue/pqAlarm.cpp
+++ priorityqueue/pqAlarm.cpp
@@ -1,109 +1,73 @@
-#include <iostream>
-#include <stdlib.h>
-#include <queue>
-#include <chrono>
-using namespace std;
-using namespace chrono;
+#include<iostream>
+#include<stdlib.h>
+#include<vector>
+#include<queue>
+#include<chrono>
+#include<random>
+using namespace std; 
+using namespace chrono; 
 
-//Clase de alarma 
+
 class Alarm{
   public:
-  //Establece la estructura de la alarma
-    struct Node{ 
-      int duration;   
-      int day;
-      int hour;
-      Node(int dt, int d, int h):duration(dt), day(d), hour(h){}; //Constructor
+    struct Node{
+      long duration;
+      Node(long dt):duration(dt){};
     };
-    void addAlarm(int quantity);//Agregar alarma 
-    Node nextAlarm(); //Obtener la alarma mas proxima
+    void addAlarm(int quantity, long nowdt);
+    vector<long> nextClosestAlarm(long value);
   private:
-    class Compare{ //Clase anidada para usarla a manera de comparador
+    class Compare{
       public:
-        //implementacion de functor
-        bool operator()(const Alarm::Node& a,const Alarm::Node& b ){
-          
-          //en caso de que sean alarmas atrasadas se verifican cual de dos ocurrio antes
-          return a.duration > b.duration; 
+        bool operator()(const Alarm::Node a, const Alarm::Node b){
+          return a.duration < b.duration; 
         }
-    }; 
-    priority_queue<Node, vector<Node>, Compare> pq; //se establece la priority queue 
-    int randomDuration(); // una funcion que genera una duracion aleatorio
+    };
+    priority_queue<Alarm::Node, vector<Alarm::Node>, Compare> pq; 
+    long randomDuration(long nowdt);
 };
 
+long Alarm:: randomDuration(long nowdt){
+  random_device rd; 
+  mt19937 gen(rd());
+  uniform_int_distribution<> dist(-86400, 86400);
+  long r = dist(gen);
 
-int Alarm::randomDuration()
-{
-  //consigue el epoch del tiempo actual
-  auto tp = system_clock::now();
-  auto dt = duration_cast<seconds>(tp.time_since_epoch()).count();
-  int randNum = rand() % 2 + 1; //numero aleatorio para decidir
-  int randomdt; //almacena el tiempo aleatorio de la nueva alarma
-
-  /*Utilizado para generar de manera dinamica
-  las alarmas proximas y pasadas apartir del tiempo actual
-  */ 
-  
-    randomdt = dt + rand()%259200-(259200/2); //suma al epoch actual 
-  return randomdt; 
+  return nowdt + r;  
 }
 
-void Alarm::addAlarm(int quantity)
-{
-  /*Utilizado para insertar un numero determinado de alarmas
-    en el que se establecen los valores de la estructura que se
-    indica en la clase.
-  */
-  for(int i =0; i<= quantity; i++){
-    //Almacena el tiempo del epoch para extraer dia y hora
-    time_t timeStmp= randomDuration();
-    //Establece en la estructura para representar hora y fecha desglosada
-    //Se realiza mediante un puntero para accesar a los atributos del dia y hora 
-    tm* timeStr = localtime(&timeStmp);
-    Node alarm(timeStmp, timeStr->tm_mday,  timeStr->tm_hour);
-    pq.push(alarm); 
+void Alarm::addAlarm(int quantity, long nowdt){ 
+  srand(time(0)); 
+  for(int i = 0; i < quantity; i++){
+    long dt = randomDuration(nowdt); 
+    Node alarm(dt); 
+    pq.push(alarm);
   }
 }
 
-
-Alarm::Node Alarm::nextAlarm(){
-
-  /*
-    Ya que solo regresa nodos, se coloca 
-    un mensaje de error para que no caiga
-    el programa
-   */
-  if(pq.empty())
-  {
-    //si esta vacia lanza un error
-    throw runtime_error("pila vacia");
-  
-  }
-
-  /*Se hace este proceso para 
-  sacar la proxima alerta conforme las 
-  iteraciones. 
-  */
-
-  auto tp = system_clock::now();
-  auto dt = duration_cast<seconds>(tp.time_since_epoch()).count();
-
-  while(!(pq.empty()) && pq.top().duration < dt){
-    cout<<pq.size()<<endl; 
-    cout<< pq.top().duration - dt<<endl; 
-    cout<<"duracion pq"<<pq.top().duration<<" dt"<<dt<<endl;
+vector<long> Alarm::nextClosestAlarm(long nowdt){
+  vector<long> closestAlarm;
+  while(!pq.empty()){
+    if(nowdt<pq.top().duration){
+      closestAlarm.push_back(pq.top().duration);
+    }
     pq.pop(); 
   }
-  Node topNode = pq.top(); // se almacena la alarma mas proxima 
-  pq.pop(); //la elimina
-  return topNode; //regresa la alarma 
+  return closestAlarm; 
 }
 
-int main()
-{
+int main(){
   Alarm a;
-  a.addAlarm(5);
-  auto alarm = a.nextAlarm(); 
-  cout<< alarm.duration; 
+  vector<long> nextAlarm; 
 
+  auto tp = system_clock::now();
+  auto dt = duration_cast<seconds>(tp.time_since_epoch()).count();
+  
+  a.addAlarm(10000,dt);
+  nextAlarm = a.nextClosestAlarm(dt); 
+
+  for(int i = 0; i < nextAlarm.size(); i++)
+  {
+    cout<<nextAlarm[i]<<endl; 
+  }
 }
